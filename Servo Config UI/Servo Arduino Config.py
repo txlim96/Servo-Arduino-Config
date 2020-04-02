625f3afddcf66678b52b340340e65abadeb59e5b
txlim96 <30485660+txlim96@users.noreply.github.com>
2020-04-02 23:21:40 +0800

    Add files via upload
    Servo Config GUI v1.2.1:
    - Simplified code by classify all similar widgets into lists
    
    Method used:
    - widget.config(command=lambda idx=i:<function(idx)>)
        ~ parse i to idx
        ~ parse idx to function
    - append to list after configs

--- Servo Config UI/Servo Arduino Config.py
+++ Servo Config UI/Servo Arduino Config.py
@@ -16,137 +16,108 @@ class ServoArduinoUI(object):
         changePin = True
         servoState = 'normal'
         angleState = 'disabled'
+
+        self.spinServo = []         # list of servo spinners
+        self.spinAngle = []         # list of angle spinners
+        self.btnServoTest = []      # list of test buttons
+        self.btnSetHome = []        # list of set home buttons
+
+        self.frame = []
+
+        for i in range(4):
+            frame = Frame(self.master, bg='black')
+            self.frame.append(frame)
+        
         self.frameTop1()
-        self.frameTop2()
-        self.frameTop3()
+        self.frameTop23()
         self.frameTop4()
         self.frameTop5()
 
         self.strReturnHomeText = StringVar()
         self.strReturnHomeText.set("Return home")
         self.btnReturnHome = Button(self.master, highlightbackground='black', fg='white', textvariable=self.strReturnHomeText, command=self.execHome)
-        self.btnReturnHome.pack(fill=BOTH, pady=10)
+        self.btnReturnHome.pack(fill=BOTH, padx=10, pady=10)
 
     def exit(self, event):
         com.closeSerial()
         self.master.quit()
 
     def frameTop1(self):
-        self.frame1 = Frame(self.master)
-        self.frame1.config(bg='black')
+##        self.frames[0] = Frame(self.master, bg='black')
 
-        self.labelCom = Label(self.frame1, text="COM:")
+        self.labelCom = Label(self.frame[0], text="COM:")
         self.labelCom.config(bg='black', fg='white')
         self.labelCom.pack(side=LEFT)
 
         self.comString = StringVar()
         self.refresh()
-        self.popUpMenu = OptionMenu(self.frame1, self.comString, *self.comPort, command=self.refresh)
+        self.popUpMenu = OptionMenu(self.frame[0], self.comString, *self.comPort, command=self.refresh)
         self.popUpMenu.config(bg='black', fg='white')
         self.popUpMenu.pack(side=LEFT)
 
         self.frameTopSub1()
 
-        self.btnServoPin = StringVar()
-        self.btnServoPin.set("Confirm pins")
-        self.changePin = Button(self.frame1, wraplength=70, highlightbackground='black', fg='white', textvariable=self.btnServoPin, command=self.changeServoPin)
+        self.strServoPin = StringVar()
+        self.strServoPin.set("Confirm pins")
+        self.changePin = Button(self.frame[0], wraplength=70, highlightbackground='black', fg='white', textvariable=self.strServoPin, command=self.changeServoPin)
         self.changePin.pack(side=LEFT, fill=BOTH)
         
-        self.frame1.pack(side=TOP, fill=X, padx=150, pady=5)
+        self.frame[0].pack(side=TOP, fill=X, padx=150, pady=5)
 
     def frameTopSub1(self):
-        self.subFrame1 = Frame(self.frame1)
+        self.subFrame1 = Frame(self.frame[0])
         
-        self.btnConnectText = StringVar()
-        self.btnConnectText.set("Connect")
-        self.btnConnect = Button(self.subFrame1, highlightbackground='black', fg='white', textvariable=self.btnConnectText, command=self.connect)
+        self.strConnectText = StringVar()
+        self.strConnectText.set("Connect")
+        self.btnConnect = Button(self.subFrame1, highlightbackground='black', fg='white', textvariable=self.strConnectText, command=self.connect)
         self.btnConnect.pack(side=TOP, fill=X)
 
-        self.btnRefreshText = StringVar()
-        self.btnRefreshText.set("Refresh")
-        self.btnRefresh = Button(self.subFrame1, highlightbackground='black', fg='white', textvariable=self.btnRefreshText, command=self.refresh)
+        self.btnRefresh = Button(self.subFrame1, highlightbackground='black', fg='white', text='Refresh', command=self.refresh)
         self.btnRefresh.pack(side=TOP, fill=X)
 
         self.subFrame1.pack(side=LEFT)
         
-    def frameTop2(self):
-        global servoState
-        self.frame2 = Frame(self.master)
-        self.frame2.config(bg='black')
+    def frameTop23(self):
+        global servoState, angleState
+        string = ['SERVO', 'ANGLE']
 
-        self.frameSpinner(self.frame2, "SERVO", 1)
-        self.spinbox1 = Spinbox(self.frame2, values=[3,5,6,9,10,11], width=3, bg='black', fg='white', state=servoState, command=lambda:com.sendData('S', '0', self.spinbox1.get()))
-        self.spinbox1.pack(side=LEFT)
-        self.frameSpinner(self.frame2, "SERVO", 2)
-        self.spinbox2 = Spinbox(self.frame2, values=[3,5,6,9,10,11], width=3, bg='black', fg='white', state=servoState, command=lambda:com.sendData('S', '1', self.spinbox2.get()))
-        self.spinbox2.pack(side=LEFT)
-        self.frameSpinner(self.frame2, "SERVO", 3)
-        self.spinbox3 = Spinbox(self.frame2, values=[3,5,6,9,10,11], width=3, bg='black', fg='white', state=servoState, command=lambda:com.sendData('S', '2', self.spinbox3.get()))
-        self.spinbox3.pack(side=LEFT)
-        self.frameSpinner(self.frame2, "SERVO", 4)
-        self.spinbox4 = Spinbox(self.frame2, values=[3,5,6,9,10,11], width=3, bg='black', fg='white', state=servoState, command=lambda:com.sendData('S', '3', self.spinbox4.get()))
-        self.spinbox4.pack(side=LEFT)
-        
-        self.frame2.pack(side=TOP, fill=X, pady=5)
+        for f in range(1, 3):
+            for i in range(4):
+                self.labelSpinner = Label(self.frame[f], text=string[f-1]+ ' '+str(i), bg='black', fg='white')
+                self.labelSpinner.pack(side=LEFT, fill=X, expand=True)
 
-    def frameTop3(self):
-        global angleState
-        self.frame3 = Frame(self.master)
-        self.frame3.config(bg='black')
+                spinbox = Spinbox(self.frame[f], width=3, bg='black', fg='white')
+                
+                if f == 1:
+                    spinbox.config(values=[3,5,6,9,10,11], state=servoState, command=lambda idx=i:com.sendData('S', str(idx), self.spinServo[idx].get()))
+                    self.spinServo.append(spinbox)
+                    self.spinServo[i].pack(side=LEFT)
+                elif f == 2:
+                    spinbox.config(from_=0, to=180, state=angleState, command=lambda idx=i:com.sendData('', str(idx), self.spinAngle[idx].get()))
+                    self.spinAngle.append(spinbox)
+                    self.spinAngle[i].pack(side=LEFT)
 
-        self.frameSpinner(self.frame3, "ANGLE", 1)
-        self.spinbox5 = Spinbox(self.frame3, from_=0, to=180, width=3, bg='black', fg='white', state=angleState, command=lambda:com.sendData('', '0', self.spinbox5.get()))
-        self.spinbox5.pack(side=LEFT)
-        self.frameSpinner(self.frame3, "ANGLE", 2)
-        self.spinbox6 = Spinbox(self.frame3, from_=0, to=180, width=3, bg='black', fg='white', state=angleState, command=lambda:com.sendData('', '1', self.spinbox6.get()))
-        self.spinbox6.pack(side=LEFT)
-        self.frameSpinner(self.frame3, "ANGLE", 3)
-        self.spinbox7 = Spinbox(self.frame3, from_=0, to=180, width=3, bg='black', fg='white', state=angleState, command=lambda:com.sendData('', '2', self.spinbox7.get()))
-        self.spinbox7.pack(side=LEFT)
-        self.frameSpinner(self.frame3, "ANGLE", 4)
-        self.spinbox8 = Spinbox(self.frame3, from_=0, to=180, width=3, bg='black', fg='white', state=angleState, command=lambda:com.sendData('', '3', self.spinbox8.get()))
-        self.spinbox8.pack(side=LEFT)
-
-        self.frame3.pack(side=TOP, fill=X, pady=5)
+            self.frame[f].pack(side=TOP, fill=X, pady=5)
 
     def frameTop4(self):
-        self.frame4 = Frame(self.master)
-        self.frame4.config(bg='black')
+        self.frame4 = Frame(self.master, bg='black')
 
-        self.btnServoText = StringVar()
-        self.btnServoText.set("Test")
-        self.btnServoTest1 = Button(self.frame4, highlightbackground='black', fg='white', textvariable=self.btnServoText, command=lambda:com.sendData(mode='T', val='0'))
-        self.btnServoTest1.pack(side=LEFT, fill=X, expand=True, padx=20)
-        self.btnServoTest2 = Button(self.frame4, highlightbackground='black', fg='white', textvariable=self.btnServoText, command=lambda:com.sendData(mode='T', val='1'))
-        self.btnServoTest2.pack(side=LEFT, fill=X, expand=True, padx=20)
-        self.btnServoTest3 = Button(self.frame4, highlightbackground='black', fg='white', textvariable=self.btnServoText, command=lambda:com.sendData(mode='T', val='2'))
-        self.btnServoTest3.pack(side=LEFT, fill=X, expand=True, padx=20)
-        self.btnServoTest4 = Button(self.frame4, highlightbackground='black', fg='white', textvariable=self.btnServoText, command=lambda:com.sendData(mode='T', val='3'))
-        self.btnServoTest4.pack(side=LEFT, fill=X, expand=True, padx=20)
-        
+        for i in range(4):
+            btn = Button(self.frame4, highlightbackground='black', fg='white', text='Test', command=lambda idx=i:com.sendData(mode='T', val=str(idx)))
+            self.btnServoTest.append(btn)
+            self.btnServoTest[i].pack(side=LEFT, fill=X, expand=True, padx=20)
+            
         self.frame4.pack(side=TOP, fill=X, pady=10)
 
     def frameTop5(self):
-        self.frame5 = Frame(self.master)
-        self.frame5.config(bg='black')
+        self.frame5 = Frame(self.master, bg='black')
 
-        self.strSetHomeText = StringVar()
-        self.strSetHomeText.set("Set home")
-        self.btnSetHome1 = Button(self.frame5, highlightbackground='black', fg='white', textvariable=self.strSetHomeText, command=lambda:self.setHome('0', self.spinbox5.get()))
-        self.btnSetHome1.pack(side=LEFT, fill=X, expand=True, padx=20)
-        self.btnSetHome2 = Button(self.frame5, highlightbackground='black', fg='white', textvariable=self.strSetHomeText, command=lambda:self.setHome('1', self.spinbox6.get()))
-        self.btnSetHome2.pack(side=LEFT, fill=X, expand=True, padx=20)
-        self.btnSetHome3 = Button(self.frame5, highlightbackground='black', fg='white', textvariable=self.strSetHomeText, command=lambda:self.setHome('2', self.spinbox7.get()))
-        self.btnSetHome3.pack(side=LEFT, fill=X, expand=True, padx=20)
-        self.btnSetHome4 = Button(self.frame5, highlightbackground='black', fg='white', textvariable=self.strSetHomeText, command=lambda:self.setHome('3', self.spinbox8.get()))
-        self.btnSetHome4.pack(side=LEFT, fill=X, expand=True, padx=20)
-        
+        for i in range(4):
+            btn = Button(self.frame5, highlightbackground='black', fg='white', text='Set home', command=lambda idx=i:self.setHome(str(idx), self.spinAngle[idx].get()))
+            self.btnSetHome.append(btn)
+            self.btnSetHome[i].pack(side=LEFT, fill=X, expand=True, padx=20)
+
         self.frame5.pack(side=TOP, fill=X)
-
-    def frameSpinner(self, frame, string, idx):
-        self.labelSpinner = Label(frame, text=string+' '+str(idx))
-        self.labelSpinner.config(bg='black', fg='white')
-        self.labelSpinner.pack(side=LEFT, fill=X, expand=True)
 
     def setHome(self, num, v):
         self.home[num] = str(v)
@@ -163,21 +134,18 @@ class ServoArduinoUI(object):
             if changePin is False:
                 servoState = 'normal'
                 angleState = 'disabled'
-                self.btnServoPin.set("Confirm pins")
+                self.strServoPin.set("Confirm pins")
             else:
                 servoState = 'disabled'
                 angleState = 'normal'
-                self.btnServoPin.set("Change servo pins")
+                self.strServoPin.set("Change servo pins")
                 
             changePin = not changePin
-            self.spinbox1.config(state=servoState)
-            self.spinbox2.config(state=servoState)
-            self.spinbox3.config(state=servoState)
-            self.spinbox4.config(state=servoState)
-            self.spinbox5.config(state=angleState)
-            self.spinbox6.config(state=angleState)
-            self.spinbox7.config(state=angleState)
-            self.spinbox8.config(state=angleState)
+            for i in range(4):
+                self.spinServo[i].config(state=servoState)
+
+            for i in range(4):
+                self.spinAngle[i].config(state=angleState)
             
         except Exception as e:
             print(e)
@@ -192,22 +160,20 @@ class ServoArduinoUI(object):
                     port = self.comString.get()
                     com.setupSerial(port)
                     connected = True        # send pin attach code here
-                    self.btnConnectText.set('Disconnect')
+                    self.strConnectText.set('Disconnect')
                     self.master.title('Servo Arduino Configuration-Connected to '+port)
                     com.sendData()
                     time.sleep(1)       # buffer period
-                    com.sendData('S', '0', self.spinbox1.get())
-                    time.sleep(1)
-                    com.sendData('S', '1', self.spinbox2.get())
-                    time.sleep(1)
-                    com.sendData('S', '2', self.spinbox3.get())
-                    time.sleep(1)
-                    com.sendData('S', '3', self.spinbox4.get())
+
+                    for i in range(4):
+                        com.sendData('S', str(i), self.spinServo[i].get())
+                        time.sleep(1)
+                        
                 else:
                     print('Disconnected')
                     com.closeSerial()
                     connected = False
-                    self.btnConnectText.set('Connect')
+                    self.strConnectText.set('Connect')
                     self.master.title('Servo Arduino Configuration')
         except Exception as e:
             print(e)
